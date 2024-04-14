2971830944cc6a3004eab7b837da84d40b894fe6
Edyta2801 <edyta.szarowska@interia.pl>
2024-04-14 18:07:45 +0200

    change url path

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,4 +1,5 @@
 export enum CONSTANTS {
-  URL = "http://proxy-patronageapi.bsolutions.usermd.net",
+  // URL = "http://proxy-patronageapi.bsolutions.usermd.net",
+  URL = "https://backend-patronage-19672f0f39ca.herokuapp.com",
   RESPONSE_SUCCESS = 200,
 }
